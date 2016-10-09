fd7eca697595178082c8431b6ba75d00d8c8f45f
tarlepp <tarmo.leppanen@protacon.com>
2016-10-09 15:28:19 +0300

    Changed for header component to show login / logout icons.

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire } from 'angularfire2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
+  constructor(
+    public af: AngularFire,
+    public router: Router
+  ) { }
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  logout() {
+    this.af.auth.logout();
+
+    this.router.navigateByUrl('/about');
   }
-
 }
