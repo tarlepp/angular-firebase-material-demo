b8fc58eb42c879c1e7f2f6db641c7169136f55b6
tarlepp <tarmo.leppanen@protacon.com>
2016-10-09 20:16:23 +0300

    Updated dependencies + added missing types.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,7 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
+declare var require: any;
+declare namespace firebase.database.ServerValue {
+  var TIMESTAMP: any
+}
