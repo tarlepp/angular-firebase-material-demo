2481e12e934b3aecf298b9b8ba1280c2ab24e617
tarlepp <tarmo.leppanen@protacon.com>
2016-10-11 21:03:33 +0300

    Added genereic purpose flex directive.

---
+++ src/app/shared/flex.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FlexDirective } from './flex.directive';
+
+describe('Directive: Flex', () => {
+  it('should create an instance', () => {
+    let directive = new FlexDirective();
+    expect(directive).toBeTruthy();
+  });
+});
