1dd1a1959b89e9831eeffdcb21e2359eeb8ff317
tarlepp <tarmo.leppanen@protacon.com>
2016-10-13 20:29:47 +0300

    Added canActivate guard to 'chat' and 'todos' routes.

--- src/app/chat/chat.routing.ts
+++ src/app/chat/chat.routing.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 
+import { AuthenticationGuard } from './../shared';
 import { ChatComponent } from './chat.component';
 
 export const ChatRoutes: Routes = [
   {
     path: 'chat',
     component: ChatComponent,
+    canActivate: [
+      AuthenticationGuard,
+    ],
   }
 ];
--- src/app/todos/list/list.routing.ts
+++ src/app/todos/list/list.routing.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 
+import { AuthenticationGuard } from './../../shared';
 import { ListComponent } from './list.component';
 
 export const TodosListRoutes: Routes = [
   {
     path: 'todos',
     component: ListComponent,
+    canActivate: [
+      AuthenticationGuard,
+    ],
   },
 ];
