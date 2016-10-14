00961e3b2c4c4a9ded14461f81fd3597744a5ccd
tarlepp <tarmo.leppanen@protacon.com>
2016-10-14 18:39:21 +0300

    Refactor main app module to use modules instead of components. Also removed imports that are in SharedModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule, MdIconRegistry } from '@angular/material';
-import * as firebase from 'firebase'; // See https://github.com/angular/angularfire2/issues/529
-import { AngularFireModule } from 'angularfire2';
-import { MomentModule } from 'angular2-moment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { config } from './config/config';
-import { ListComponent } from './todos/list/list.component';
-import { LoginComponent } from './login/login.component';
-import { ChatComponent } from './chat/chat.component';
 import { SharedModule } from './shared/shared.module';
-import { TodosResolver } from './todos/list/todos.resolver';
+import { LayoutModule } from './layout/layout.module'
+import { AboutModule } from './about/about.module';
+import { LoginModule } from './login/login.module';
+import { TodosModule } from './todos/todos.module';
+import { ChatModule } from './chat/chat.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    HeaderComponent,
-    FooterComponent,
-    ListComponent,
-    LoginComponent,
-    ChatComponent,
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    MaterialModule.forRoot(),
-    AngularFireModule.initializeApp(config.FIREBASE_CONFIG, config.FIREBASE_AUTH_CONFIG),
-    MomentModule,
     AppRoutingModule,
     SharedModule,
-  ],
-  providers: [
-    MdIconRegistry,
-    TodosResolver
+    LayoutModule,
+    AboutModule,
+    LoginModule,
+    TodosModule,
+    ChatModule,
   ],
   bootstrap: [
     AppComponent,
-  ]
+  ],
 })
 
 export class AppModule { }
