6a2f5559fedcf4f2f86e3229e03ce93cc1834044
tarlepp <tarmo.leppanen@protacon.com>
2016-10-14 18:09:00 +0300

    Added all common modules to shared module.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,15 @@
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+import { MaterialModule, MdIconRegistry } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import { MomentModule } from 'angular2-moment';
 
 import { AuthenticationModule } from './authentication/authentication.module';
 import { Directives } from './directives/';
+import { config } from '../config/config';
 
 @NgModule({
   declarations: [
@@ -10,12 +17,28 @@ import { Directives } from './directives/';
   ],
   imports: [
     CommonModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
     AuthenticationModule.forRoot(),
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(config.FIREBASE_CONFIG, config.FIREBASE_AUTH_CONFIG),
+    MomentModule,
+  ],
+  providers: [
+    MdIconRegistry,
   ],
   exports: [
     CommonModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AuthenticationModule,
+    MaterialModule,
+    AngularFireModule,
+    MomentModule,
     ...Directives,
-  ]
+  ],
 })
 
 export class SharedModule { }
