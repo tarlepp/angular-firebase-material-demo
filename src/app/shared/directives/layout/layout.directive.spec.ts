73868013c6cac9d346cc9b8dc925b573315375a5
tarlepp <tarmo.leppanen@protacon.com>
2016-10-15 21:18:50 +0300

    Added first real tests!

--- src/app/shared/directives/layout/layout.directive.spec.ts
+++ src/app/shared/directives/layout/layout.directive.spec.ts
@@ -1,11 +1,65 @@
 /* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
+import { Component } from '@angular/core/src/metadata/directives';
+import { TestBed } from '@angular/core/testing';
 import { LayoutDirective } from './layout.directive';
 
 describe('Directive: Layout', () => {
-  it('should create an instance', () => {
-    let directive = new LayoutDirective();
-    expect(directive).toBeTruthy();
+  let fixture;
+
+  it('should thrown an exception with invalid \'appLayout\' attribute value.', () => {
+    @Component({
+      selector: 'app-test-cmp',
+      template: '<div appLayout="foo"></div>',
+    })
+    class TestComponent { }
+
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        LayoutDirective,
+      ],
+    });
+
+    fixture = TestBed.createComponent(TestComponent);
+
+    expect(() => { fixture.detectChanges(); }).toThrowError();
+  });
+
+  it('should thrown an exception with invalid \'appLayoutAlign\' attribute value.', () => {
+    @Component({
+      selector: 'app-test-cmp',
+      template: '<div appLayout="column" appLayoutAlign="bar"></div>',
+    })
+    class TestComponent { }
+
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        LayoutDirective,
+      ],
+    });
+
+    fixture = TestBed.createComponent(TestComponent);
+
+    expect(() => { fixture.detectChanges(); }).toThrowError();
+  });
+
+  it('should thrown an exception with invalid \'appLayoutJustify\' attribute value.', () => {
+    @Component({
+      selector: 'app-test-cmp',
+      template: '<div appLayout="column" appLayoutJustify="foobar"></div>',
+    })
+    class TestComponent { }
+
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        LayoutDirective,
+      ],
+    });
+
+    fixture = TestBed.createComponent(TestComponent);
+
+    expect(() => { fixture.detectChanges(); }).toThrowError();
   });
 });
