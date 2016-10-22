cf6df0da3e9a3d39d9c4f3ab94497ff07d10c3aa
tarlepp <tarmo.leppanen@protacon.com>
2016-10-22 22:17:57 +0300

    Added first tests for chat/room component.

--- src/app/chat/room/room.component.spec.ts
+++ src/app/chat/room/room.component.spec.ts
@@ -1,13 +1,93 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RoomComponent } from './room.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LocalStorageService, Ng2Webstorage } from 'ng2-webstorage';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule, MdInput } from '@angular/material';
+import { MomentModule } from 'angular2-moment';
+import { By } from '@angular/platform-browser';
 
-describe('Component: Chat/Room', () => {
-  /*
-  it('should create an instance', () => {
-    // let component = new ChatComponent();
-    // expect(component).toBeTruthy();
+class StubLocalStorageService { }
+
+class StubActivatedRoute {
+  public data = {
+    subscribe: StubActivatedRoute.subscribe
+  };
+
+  static subscribe() {
+    return {
+      messages: [],
+      nick: 'foo bar',
+    };
+  }
+}
+
+describe('Component: /chat/room/room.component.ts', () => {
+  let fixture: ComponentFixture<RoomComponent>;
+
+  beforeEach(() => {
+    const fakeRouter = {
+      navigateByUrl: (url: string) => url,
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [
+        RoomComponent,
+      ],
+      imports: [
+        FormsModule,
+        Ng2Webstorage,
+        MomentModule,
+        MaterialModule.forRoot(),
+      ],
+      providers: [
+        {
+          provide: Router,
+          useValue: fakeRouter,
+        },
+        {
+          provide: ActivatedRoute,
+          useClass: StubActivatedRoute,
+        },
+        {
+          provide: LocalStorageService,
+          useClass: StubLocalStorageService,
+        },
+      ],
+    });
+
+    fixture = TestBed.createComponent(RoomComponent);
   });
-  */
+
+  it('should create the component', async(() => {
+    let component = fixture.debugElement.componentInstance;
+
+    expect(component).toBeTruthy();
+  }));
+
+  it('should not allow to click submit button if no message given (button should be disabled)', () => {
+    fixture.detectChanges();
+
+    const button: HTMLButtonElement = fixture.debugElement.query(By.css('button')).nativeElement;
+
+    expect(button.disabled).toBe(true, 'submit button is not disabled');
+  });
+
+  describe('After entering message', () => {
+    it('should allow to click submit button (button should not be disabled)', () => {
+      fixture.detectChanges();
+
+      const input: MdInput = fixture.debugElement.query(By.directive(MdInput)).componentInstance;
+      const inputElement: HTMLInputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+      const button: HTMLButtonElement = fixture.debugElement.query(By.css('button')).nativeElement;
+
+      // Fake a `change` event being triggered.
+      inputElement.value = 'awesome message!';
+      input._handleChange(<any> {target: inputElement});
+
+      fixture.detectChanges();
+
+      expect(button.disabled).toBe(false, 'submit button is not enabled');
+    });
+  });
 });
