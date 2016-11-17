65eb113b0676c6eb62c79150936a5da4b9c8ea14
tarlepp <tarmo.leppanen@protacon.com>
2016-11-17 21:13:30 +0200

    Added missing comments.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFire, AuthMethods, AuthProviders } from 'angularfire2';
+
 import { AnimationsService } from '../shared/animations/animations.service';
 import { Animations } from '../shared/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 
 export class LoginComponent extends Animations implements OnInit {
-  constructor(
+  /**
+   * Constructor of the class.
+   *
+   * @param {AnimationsService} animationsService
+   * @param {AngularFire}       angularFire
+   * @param {Router}            router
+   */
+  public constructor(
     protected animationsService: AnimationsService,
     private angularFire: AngularFire,
     private router: Router
@@ -19,7 +27,12 @@ export class LoginComponent extends Animations implements OnInit {
     super(animationsService);
   }
 
-  ngOnInit() {
+  /**
+   * ngOnInit lifecycle hook.
+   *
+   * @see https://angular.io/docs/ts/latest/guide/lifecycle-hooks.html
+   */
+  public ngOnInit() {
     this.angularFire.auth.subscribe(auth => {
       if (auth && auth.uid) {
         this.router.navigateByUrl('/about');
@@ -27,14 +40,19 @@ export class LoginComponent extends Animations implements OnInit {
     });
   }
 
-  login(provider: string) {
+  /**
+   * Method to login with specified provider.
+   *
+   * @param {string}  provider
+   */
+  public login(provider: string) {
     this.angularFire.auth
       .login({
         provider: AuthProviders[provider],
         method: AuthMethods.Popup,
       })
       .then(() => {
-        this.router.navigateByUrl('/todos');
+        this.router.navigate(['/todos']);
       })
       .catch(error => {
         alert(error);
